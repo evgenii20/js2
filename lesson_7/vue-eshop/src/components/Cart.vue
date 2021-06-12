9d7e100f23fa858dedf2dab158269b95f8ca9978
evgenii20 <zev_ps@mail.ru>
2021-06-12 14:42:02 +0500

    lesson_7_hw

---
+++ lesson_7/vue-eshop/src/components/Cart.vue
@@ -0,0 +1,67 @@
+<template>
+  
+  <div v-show="isVisibleCart" class="cart-block">
+    Корзина:
+    
+    <div class="cart-list">
+      <div v-for="item in cartProducts" :key="item.id_product" class="product-item">
+        <Wrapper>
+          <h3>{{ item.product_name }}</h3>
+        </Wrapper>
+        <p>{{ item.price }}</p>
+        <button @click="onClick(item)">Удалить</button>
+      </div>
+    </div>
+    
+  </div>
+  
+</template>
+
+<script>
+// import Wrapper from './Wrapper.vue'
+
+const API = 'http://localhost:3000';
+
+export default {
+  name: "Cart",
+  // поскольку "isVisibleCart" приходит к нам из вне, поэтому используем props
+  props: {
+    isVisibleCart: {
+      type: Boolean,
+      default: false,
+    },
+    cartProducts: {
+      type: Array,
+      default: () => ([]),
+    },
+    postJson: {
+      type: Function,
+      default: () => null,
+    },
+    getJson: {
+      type: Function,
+      default: () => null,
+    },
+  //  без описания в props: getCart не удаляет товар из корзины
+    getCart: {
+      type: Function,
+      default: () => null,
+    },
+  },
+  methods: {
+    onClick(item) {
+      // Удаление товара
+      // $emit - отлавливаем 'filter-products' с 'App.vue'
+      // this.$emit('filter-products', this.userSearch)
+      // this.postJson(`${API}/addToCart`, this.item)
+      this.postJson(`${API}/deleteFromCart`, item)
+          .then(() => this.getCart())
+      // this.$emit('toggle-cart', this.toggleCartStatus)
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ lesson_7/vue-eshop/src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <header>
+    <div class="logo">Интернет-магазин</div>
+    <div class="cart">
+      <!--      <SearchForm :products="userSearch"/>-->
+
+      <form action="#" class="search-form">
+        
+        <input type="text" class="search-field" v-model="searchValue">
+        
+        <button class="btn-search" type="submit" @click="onClick">Найти
+          
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+
+      <button class="btn-cart" type="button" @click="$emit('toggle-cart')">Корзина</button>
+      
+      <!-- вынес в Cart.vue -->
+      
+    </div>
+    
+  </header>
+</template>
+
+<script>
+// import SearchForm from "./SearchForm";
+// import Cart from "./Cart";
+
+// import SearchForm from "./SearchForm";
+
+export default {
+  name: "Header",
+  // components: {SearchForm},
+  // components: {Cart},
+  data: () => ({
+    // Header,
+    // SearchForm,
+    // userSearch: '',
+    searchValue: '',
+  }),
+
+  methods: {
+    onClick() {
+      // $emit - отлавливаем 'filter-products' с 'App.vue'
+      // this.$emit('filter-products', this.userSearch)
+      this.$emit('filter-products', this.searchValue)
+      // this.$emit('toggle-cart', this.toggleCartStatus)
+    },
+  },
+  watch: {
+    //searchLine
+    // userSearch() {
+    searchValue() {
+      // filterGoods
+      // this.filters();
+      this.onClick();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
